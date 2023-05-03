fe3f83d29bae4660e0bc82eadfe1326524538da7
Anton Toneyev <aatoneyev@gmail.com>
2023-05-03 17:56:04 +0300

    update2

--- todoAppReact/todoAppReact/src/App.jsx
+++ todoAppReact/todoAppReact/src/App.jsx
@@ -1,21 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-//import './assets/global.css'
+import './assets/global.css'
 import './Footer.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div className='todoapp'>
-        <h1>
-          todos 
-        </h1>
-        <todoInput/>
-      </div>
-    </>
+    <div>
+			<header className="header">
+				<h1>todos</h1>
+				{todoInput}
+			</header>
+		</div>
   )
 }
 
