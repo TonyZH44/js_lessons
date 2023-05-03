8a343163c7c34e8b1d6600f88a82321330f39cd5
Anton Toneyev <aatoneyev@gmail.com>
2023-05-03 17:33:54 +0300

    update 1

--- todoAppReact/todoAppReact/src/App.jsx
+++ todoAppReact/todoAppReact/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './assets/global.css'
+import './Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='todo'>
+      <div className='todoapp'>
         <h1>
-          todos
+          todos 
         </h1>
-        
+        <todoInput/>
       </div>
     </>
   )
--- todoAppReact/todoAppReact/src/TodoInput.jsx
+++ todoAppReact/todoAppReact/src/TodoInput.jsx
@@ -1,1 +1,17 @@
-const TodoInput = ({})
+
+
+function todoInput() {
+
+    return (
+        <div>
+            <input
+                className="new-todo"
+                placeholder="What needs to be done?"
+                value={this.state.newTodo}
+                onKeyDown={this.handleNewTodoKeyDown}
+                onChange={this.handleChange}
+                autoFocus={true}
+            />
+        </div>
+    );
+}
